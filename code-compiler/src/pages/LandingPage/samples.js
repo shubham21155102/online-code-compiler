da265fb4e706681881f8d5d305bd5bf87829ae9d
shubham <shubham@shubhams-MacBook-Air.local>
2023-09-02 02:17:09 +0530

    frontend implemented

--- code-compiler/src/pages/LandingPage/samples.js
+++ code-compiler/src/pages/LandingPage/samples.js
@@ -1,14 +1,13 @@
-const samples = {};
-
-samples.cpp = `#include <iostream>
-using namespace std;
-int main() {
-    cout << "Hello Shubham";
-    return 0;
-}
-`;
-
-samples.python = `# python3
-print("Hello Shubham !!!")`;
-
-export default samples;
+const samples = {
+    cpp: `#include <iostream>
+  using namespace std;
+  int main() {
+      cout << "Hello Shubham";
+      return 0;
+  }`,
+    python: `# python3
+  print("Hello Shubham !!!")`,
+  };
+  
+  export default samples;
+  
